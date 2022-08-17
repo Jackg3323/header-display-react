bcaf4dd38179ee78654b08da38e5e330fe865bbb
Jack Guthrie <jackg3323@gmail.com>
2022-08-17 15:23:48 +0100

    Add profile component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
+import Profile from "./components/Profile/Profile";
 import Title from "./components/Title/Title";
+
+const imgUrl = "https://i.imgur.com/yXOvdOSs.jpg";
 
 function App() {
   const [name, setName] = useState("User");
@@ -11,6 +14,7 @@ function App() {
       <Title />
       <Header name={name} />
       <Form setName={setName} />
+      <Profile name={name} imgUrl={imgUrl} />
     </>
   );
 }
