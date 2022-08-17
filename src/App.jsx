75619840e237d4a39c4ac53191641a1e87dd862d
Jack Guthrie <jackg3323@gmail.com>
2022-08-17 14:11:30 +0100

    Add form and make header dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form/Form";
 import Header from "./components/Header/Header";
+import Title from "./components/Title/Title";
 
 function App() {
-  const name = useState("Mark");
+  const [name, setName] = useState("User");
   return (
     <>
+      <Title />
       <Header name={name} />
-      <Form />
+      <Form setName={setName} />
     </>
   );
 }
--- src/Components/Form/Form.jsx
+++ src/Components/Form/Form.jsx
@@ -1,13 +1,38 @@
-export default function Form() {
+import PropTypes from "prop-types";
+
+export default function Form({ setName }) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Form Submitted");
+    const form = event.target;
+    const name = form.name.value;
+    console.log(`Hello ${name}`);
+    // const submission = Object.fromEntries(new FormData(event.target));
+    setName(name);
+  };
+
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit}
+      className="mx-auto w-80 flex-col items-center"
+    >
       <div>
         <label htmlFor="name" className="sr-only">
           Name
         </label>
-        <input type="text" placeholder="Enter Your Name" id="name" />
-        <button type="submit">Submit</button>
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          id="name"
+          className="rounded border pl-2"
+          name="name"
+        />
       </div>
+      <button type="submit" className="mt-4 bg-purple-500 px-4 py-2 text-white">
+        Submit
+      </button>
     </form>
   );
 }
+
+Form.propTypes = { setName: PropTypes.func.isRequired };
